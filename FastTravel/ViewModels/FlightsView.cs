85958a87d91ff7e03f59f355d6824d728377165b
ClownOfMadness <52707645+ClownOfMadness@users.noreply.github.com>
2023-01-10 16:02:22 +0200

    update: added select plane

--- FastTravel/ViewModels/FlightsView.cs
+++ FastTravel/ViewModels/FlightsView.cs
@@ -6,12 +6,14 @@ namespace FastTravel.ViewModels
     {
         public IEnumerable<Port> ports { get; set; }
         public IEnumerable<Flight> flights { get; set; }
+        public IEnumerable<Plane> planes { get; set; }
         public Flight flight { get; set; }
 
         public FlightsView()
         {
             ports = new List<Port>();
             flights = new List<Flight>();
+            planes = new List<Plane>();
             flight = new Flight();
         }
     }
